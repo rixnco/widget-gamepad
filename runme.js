ad139ac24587709640eb30f05fb5d6800d39958b
RxNcO <rxboucher@gmail.com>
2019-12-06 17:00:18 +0100

    Made some changes to ChiliPeppr widget using Cloud9

--- runme.js
+++ runme.js
@@ -1393,7 +1393,11 @@ var pushToGithubSync = function(message) {
   stdout += "> git add *\n";
   stdout += '> git commit -m "' + message + '"\n';
   stdout += "> git push\n";
-  stdout += proc.execSync('git add * && git commit -m "' + message + '" && git push', { encoding: 'utf8' });
+  try {
+    stdout += proc.execSync('git add * && git commit -m "' + message + '" && git push', { encoding: 'utf8' });
+  } catch(err) {
+    
+  }
   console.log("Pushed to github sync. Stdout:", stdout);
   
   return stdout;
